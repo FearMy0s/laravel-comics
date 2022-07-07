f1e3df337505656e3b98412b9c1cb0a0e65d53aa
FearMy0s <mirko.giannetta96@gmail.com>
2022-07-07 18:48:04 +0200

    attendo spiegazioni domani ci sto provando dalle 2

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,22 @@
-<h1>Comics</h1>
+<header class="header">
+    <div class="container">
+        <div>
+            <img class="header-logo" src="{{asset('img/dc-logo.png')}}" alt="">
+        </div>
+        <nav class="header-nav">
+            <ul>
+                <li><a href="">CHARACTERS</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li><a href="">COLLECTIBLES</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
+            </ul>
+        </nav>
+    </div>
+
+</header>
