f0f779df9aaa52e400a8bb7323f4e6ee27ef9697
FearMy0s <mirko.giannetta96@gmail.com>
2022-07-07 18:48:10 +0200

    push

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,5 +4,15 @@
     Home Page
 @endsection
 @section('page-content')
-    contenuto
+    <section>
+    <div class="container">
+        <ul class= "books-container">
+            @foreach($comics as $comics)
+                <li class="books">
+                    <img src="{{$comics['thumb']}}" alt="">
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    </section>
 @endsection
