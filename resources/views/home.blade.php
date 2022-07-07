1c6ce923a048d2f81a7c568730b0897b554fc5d9
FearMy0s <mirko.giannetta96@gmail.com>
2022-07-07 16:54:53 +0200

    fixed blade problem

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.base')
+
+@section('page-title')
+    Home Page
+@endsection
+@section('page-content')
+    contenuto
+@endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('page-title')</title>
+    <title>@yield('page-title', 'DC Comics')</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    
+    @include('partials.header')
+    <main>
+    @yield('page-content')
+    </main>
+    @include('partials.footer')
 </body>
 </html>
